2791e1e674a41e3da51e4fe867705cd3ee95dd4e
Krotov Anatoliiy <tolumbas@gmail.com>
2018-01-21 12:41:29 +0200

    ADD: WooLayoutSwitch javascript-plugin

--- wp-content/themes/your-theme/extensions/woocommerce/assets/js/scripts.js
+++ wp-content/themes/your-theme/extensions/woocommerce/assets/js/scripts.js
@@ -93,3 +93,122 @@
 		setToggle('.navbar-toggle');
 	});
 })(jQuery, document, window)
+
+
+/** Plugin to switch layout classes depending on window resolution */
+function WooLayoutSwitch( options ){
+	var options = options||{};
+
+	this.settings = this.extend( this.defaults, options );
+	this.elem = document.querySelector( this.settings.selector );
+
+	if( this.elem ){
+		this.init();
+	}
+}
+
+WooLayoutSwitch.prototype = {
+	name: 'WooLayoutSwitch',
+	elem: null,
+	settings: {},
+	defaults: {
+		switchPoints : {
+			0:{ items:1 },
+			480:{ items:2 },
+			992:{ items:3 },
+			1200:{ items:4 },
+		},
+		timeout: 50,
+		selector: 'div.woocommerce[class*="columns-"]',
+		prefix: 'columns-',
+	},
+	extend: function(...rest) {
+		var result = {};
+
+		for( var index in rest ){
+			var props = rest[index];
+
+			for ( var prop in props ){
+				result[prop] = props[prop];
+			}
+		}
+
+		return result;
+	},
+	addListeners: function(){
+		window.addEventListener( 'resize', this.onResize.bind(this) );
+	},
+	removeListeners: function(){
+		window.removeEventListener( 'resize', this.onResize.bind(this) );
+	},
+	onResize: function( e ){
+		if( this.timeoutID ){
+			clearTimeout( this.timeoutID );
+		}
+		this.timeoutID = setTimeout( this.doSwitchLayout.bind( this, e ), this.settings.timeout );
+	},
+	doSwitchLayout: function( e ){
+		var self = this,
+		winw = window.outerWidth,
+		switchPoint;
+
+		for( var resolution in this.settings.switchPoints ){
+			if( winw >= parseInt( resolution ) ){
+				switchPoint = this.settings.switchPoints[resolution];
+			}
+		}
+
+		if( 'items' in switchPoint ){
+			
+			var classes = self.elem.classList;
+
+			classes.forEach( function( _class, index ){
+				if ( _class.search( RegExp(self.settings.prefix) ) >= 0 ){
+					var new_class = self.settings.prefix + switchPoint.items;
+					classes.replace( _class, new_class );
+				}
+			} );
+
+			self.elem.querySelectorAll( '.last' )
+			.forEach( function( el, index ){
+				el.classList.remove( 'last' );
+			});
+			self.elem.querySelectorAll( '.first' )
+			.forEach( function( el, index ){
+				el.classList.remove( 'first' );
+			});
+
+			self.elem.querySelectorAll( 'li.product:nth-child(' + switchPoint.items + 'n)' )
+			.forEach( function(el, index){
+				el.classList.add( 'last' );
+			});
+			
+			self.elem.querySelectorAll( 'li.product:nth-child(' + switchPoint.items + 'n+1)' )
+			.forEach( function(el, index){
+				el.classList.add( 'first' );
+			});
+		}
+	},
+	init: function() {
+		this.removeListeners();
+		this.addListeners();
+	},
+}
+
+var opts = {};
+/** Make sure that the related products section follows the same layout rules */
+if( document.body.classList.contains( 'single-product' ) ){
+	opts = {
+		switchPoints : {
+			0:{ items:1 },
+			480:{ items:2 },
+			992:{ items:4 },
+			1200:{ items:5 },
+		},
+		timeout: 50,
+		selector: 'div.woocommerce[class*="columns-"]',
+		prefix: 'columns-',
+	}
+}
+var wooLayoutSwitch = new WooLayoutSwitch( opts );
+window.dispatchEvent( new Event( 'resize' ) );
