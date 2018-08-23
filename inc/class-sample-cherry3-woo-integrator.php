97906f042e3218ab84bce469e2176dcb0152f41c
Tolumba <tolumbas@gmail.com>
2018-08-23 16:36:42 +0300

    Revert "UPD: Integrator"
    This reverts commit 3f9ead3005bd144bec1bad7f015e71caff272256.

--- inc/class-sample-cherry3-woo-integrator.php
+++ inc/class-sample-cherry3-woo-integrator.php
@@ -6,15 +6,6 @@ namespace extensions\woocommerce\inc {
 		
 		public function __construct(){
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 999 );
-
-			/**
-			 * Titles format override
-			 */
-			/*remove_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
-			add_action( 'woocommerce_shop_loop_item_title', array( $this, 'woocommerce_template_loop_product_title' ), 10 );
-
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-			add_action( 'woocommerce_single_product_summary', array( $this, 'woocommerce_template_single_title'), 5 );*/
 		}
 
 		public function output_content_wrapper_start(){ ?>
@@ -46,28 +37,10 @@ namespace extensions\woocommerce\inc {
 		<?php }
 
 		public function enqueue_scripts(){
-			/* Removes Conflict in Cherry3-base vs Wishlist plugin */
-			wp_dequeue_style( 'bootstrap-grid' );
-		}
-
-		/**
-		 * Title format in products loop
-		 * @return [type] [description]
-		 */
-		public function woocommerce_template_loop_product_title(){
-			\the_title( '<h4 class="woocommerce-loop-product__title">', '</h4>' );
-		}
-
-		/**
-		 * Title format in single product page
-		 * @return [type] [description]
-		 */
-		public function woocommerce_template_single_title(){
-			\the_title( '<h3 class="product_title entry-title">', '</h3>' );
+			
 		}
 	}
 }
-
 /**
  *  Using global namespace
  */
@@ -79,16 +52,6 @@ namespace {
 		 */
 		function is_template_shop(){
 			return 'template-shop' === basename( get_page_template(), '.php' );
-		}
-	}
-
-	if( !function_exists( 'is_shop_page' ) ){
-		/**
-		 * Checks whenever Shop-related page is displayed
-		 * @return boolean true if is single product page or is shop page or shows a taxonomy of a product post type
-		 */
-		function is_shop_page(){
-			return is_singular('product') || is_shop() || is_tax( get_object_taxonomies('product') );
 		}
 	}
 }
